8d3b1906ffe642fed0ffa1d59831dbeca820d5e9
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2024-09-11 02:47:03 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SYNCLOCKVER
+
+![npm](https://img.shields.io/npm/v/synclockver)
+![downloads](https://img.shields.io/npm/dw/synclockver)
+![license](https://img.shields.io/npm/l/synclockver)
+
+Sync package-lock.json version to package.json
+
+## Usage
+
+```bash
+npx synclockver
+```
+
+## License
+
+MIT
---
+++ index.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import path from "path";
+
+const packageJsonPath = path.join(process.cwd(), "package.json");
+const packageLockPath = path.join(process.cwd(), "package-lock.json");
+if (fs.existsSync(packageLockPath) && fs.existsSync(packageJsonPath)) {
+  try {
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+    const packageLock = JSON.parse(fs.readFileSync(packageLockPath, "utf-8"));
+    packageLock.version = packageJson.version;
+    packageLock.packages[""].version = packageJson.version;
+    fs.writeFileSync(packageLockPath, JSON.stringify(packageLock, null, 2));
+
+    const packages = fs.readdirSync(path.resolve(process.cwd(), "packages"));
+    packages.forEach((pkgDir) => {
+      try {
+        const packagePath = path.join(pkgDir, "package.json");
+        if (!fs.existsSync(packagePath)) return;
+        const packageJson = JSON.parse(fs.readFileSync(packagePath, "utf-8"));
+        packageLock.packages[`packages/${packageJson.name}`].version =
+          packageJson.version;
+        fs.writeFileSync(packageLockPath, JSON.stringify(packageLock, null, 2));
+      } catch (error) {}
+    });
+  } catch (error) {}
+}
